c055a872a46827b716cdb0c340b708a6b495ab26
5710451398 <pinjuta.t@ku.th>
2018-08-30 23:20:48 +0700

    trying to display image and json static file but the internet's down at the moment :(

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	# http://127.0.0.1:8000/___/
 	url(r'^$', views.index, name='index'),
+
+	# http://127.0.0.1:8000/books/100/
+	url(r'^(?P<book_id>[0-9]+)/$', views.detail, name='detail')
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from .models import Book
+
 
 def index(request):
-	return HttpResponse("<h1>Hello</h1>")
-# Create your views here.
+	all_books = Book.objects.all()
+	# context = {'all_books': all_books}
+	return render(request, 'books/index.html', {'all_books': all_books})
+
+
+def detail(request, book_id):
+	try:
+		book = Book.objects.get(pk=book_id)
+	except Book.DoesNotExist:
+		raise Http404("Book does not exist")
+	return render(request, 'books/detail.html', {'book': book})
